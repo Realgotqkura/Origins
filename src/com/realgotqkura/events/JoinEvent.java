867100605697a02c76287ea5b26a162046f346d4
Realgotqkura <vladianastasov06@gmail.com>
2021-07-10 18:08:57 +0300

    Bug fix with joining

--- src/com/realgotqkura/events/JoinEvent.java
+++ src/com/realgotqkura/events/JoinEvent.java
@@ -1,7 +1,7 @@
 package com.realgotqkura.events;
 
 import com.realgotqkura.gui.OriginsGUI;
-import com.realgotqkura.origin.Halo;
+import com.realgotqkura.origin.*;
 import com.realgotqkura.origins.OriginsMain;
 import com.realgotqkura.utils.ColorOf;
 import org.bukkit.entity.Player;
@@ -26,8 +26,76 @@ public class JoinEvent implements Listener {
                 if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Halo")){
                     new Halo().EquipHaloItem(player);
                     new Halo().HaloRunnable(player);
+                    return;
                 }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Elytrian")){
+                    new Elytrian().ElyrianRunnable(player);
+                    new Elytrian().ElytrianItems(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Avian")){
+                    new Avian().AvianRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Molian")){
+                    new Molian().MolianRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Ironian")){
+                    new Ironian().IronianRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Merlin")){
+                   new Merlin().MerlinRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Feline")){
+                    new Feline().FelineRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Stellar")){
+                    new Stellar().StellarRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Blazeborn")) {
+                    new Blazeborn().BlazebornRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Enderian")){
+                   new Enderian().EquipEnderianItem(player);
+                   new Enderian().EnderianRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Arachnid")){
+                    new Arachnid().ArachnidRunnable(player);
+                    new Arachnid().EquipArachnidItem(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Shulk")){
+                    new Shulk().ShulkRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Robotics")){
+                    new Robotics().RoboticsRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Wanderer")){
+                    new Wanderer().WandererRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Explorer")){
+                    new Explorer().ExplorerRunnable(player);
+                    return;
+                }
+                if(main.getConfig().getString("Origins." + player.getUniqueId().toString() + ".Origin").equals("Leprechaun")){
+                    new Leprechaun().LeprechaunRunnable(player);
+                    return;
+                }
+
                 return;
+            }else{
+                gui.createGUI(player);
+                player.sendMessage(ColorOf.colorOf("&a&lPick Your Origin &b&l" + player.getName()));
             }
         }catch(Exception e){
             gui.createGUI(player);
